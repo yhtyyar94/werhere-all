019735aa2b28864a7b3b7ebf32cc02d9943b48b9
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-11-25 12:58:25 +0100

    install and configure chakra ui

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["@chakra-ui/react"],
+  },
 };
 
 export default nextConfig;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,10 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { Provider } from "@/components/ui/provider";
+import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }

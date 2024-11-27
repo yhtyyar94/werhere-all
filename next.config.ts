0f0cf2c2c555e3d4cccb184d5da2fd46e432e042
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-11-27 09:59:30 +0100

    node crypto

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,28 @@ const nextConfig: NextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: isProduction,
   },
+  webpack: (config, { isServer, buildId, dev, webpack }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        stream: require.resolve("stream-browserify"),
+        crypto: require.resolve("crypto-browserify"),
+      };
+
+      config.plugins.push(
+        new webpack.ProvidePlugin({
+          process: "process/browser",
+        }),
+        new webpack.NormalModuleReplacementPlugin(
+          /node:crypto/,
+          (resource: any) => {
+            resource.request = resource.request.replace(/^node:/, "");
+          }
+        )
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;

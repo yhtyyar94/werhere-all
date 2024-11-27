255bd828284d57584ac238a715e22395acd58419
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-11-27 09:54:26 +0100

    disable eslint

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: isProduction,
   },
 };
 

ad2c19ce4b54ba606f65f2457e062b1f99c64f37
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-11-27 10:20:49 +0100

    disable middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,34 +3,34 @@ import { NextRequest, NextResponse } from "next/server";
 
 import * as crypto from "node:crypto";
 
-import { getIronSession } from "iron-session";
+// import { getIronSession } from "iron-session";
 
-// 1. Specify protected and public routes
-const protectedRoutes = ["/admin"];
-const publicRoutes = ["/login", "/"];
+// // 1. Specify protected and public routes
+// const protectedRoutes = ["/admin"];
+// const publicRoutes = ["/login", "/"];
 
 export default async function middleware(req: NextRequest, res: NextResponse) {
   // 2. Check if the current route is protected or public
-  const path = req.nextUrl.pathname;
-  const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoute = publicRoutes.includes(path);
-  console.log();
-  const session = await getIronSession(req, res, {
-    password: process.env.SECRET_COOKIE_PASSWORD as string,
-    cookieName: "werhere",
-    // cookieOptions: {
-    //   secure: process.env.NODE_ENV === "production",
-    // },
-  });
-  console.log(session);
-  // @ts-ignore
-  const isUserLoggedIn = session && session.username ? true : false;
+  // const path = req.nextUrl.pathname;
+  // const isProtectedRoute = protectedRoutes.includes(path);
+  // const isPublicRoute = publicRoutes.includes(path);
+  // console.log();
+  // const session = await getIronSession(req, res, {
+  //   password: process.env.SECRET_COOKIE_PASSWORD as string,
+  //   cookieName: "werhere",
+  //   // cookieOptions: {
+  //   //   secure: process.env.NODE_ENV === "production",
+  //   // },
+  // });
+  // console.log(session);
+  // // @ts-ignore
+  // const isUserLoggedIn = session && session.username ? true : false;
 
-  if (isProtectedRoute && !isUserLoggedIn) {
-    const url = req.nextUrl.clone();
-    url.pathname = "/login";
-    return NextResponse.rewrite(url);
-  }
+  // if (isProtectedRoute && !isUserLoggedIn) {
+  //   const url = req.nextUrl.clone();
+  //   url.pathname = "/login";
+  //   return NextResponse.rewrite(url);
+  // }
 
   return NextResponse.next();
 }

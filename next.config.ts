44e38273cd563a56affc47a7f9043bda77bb58c9
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-11-27 10:24:08 +0100

    nextconf

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import webpack from "webpack";
 
 const isProduction = process.env.NODE_ENV === "production";
 
@@ -13,28 +12,6 @@ const nextConfig: NextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: isProduction,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        stream: require.resolve("stream-browserify"),
-        crypto: require.resolve("crypto-browserify"),
-      };
-
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "node:crypto": "crypto-browserify",
-        "node:stream": "stream-browserify",
-      };
-
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          process: "process/browser",
-        })
-      );
-    }
-    return config;
   },
 };
 

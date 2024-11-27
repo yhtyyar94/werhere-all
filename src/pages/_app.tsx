eb1a365d0bdf8c00de2ab9c65c57f00c091842dc
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-11-27 09:41:24 +0100

    delete chakra

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,5 @@
-import { Provider } from "@/components/ui/provider";
 import { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Provider>
-      <Component {...pageProps} />
-    </Provider>
-  );
+  return <Component {...pageProps} />;
 }

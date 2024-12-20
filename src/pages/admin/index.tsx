da80ccec4329c267e0addb4b766f84c79b8589c6
Yhtyyar Annayev <70913322+yhtyyar94@users.noreply.github.com>
2024-12-20 12:27:42 +0100

    add logout button

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,7 @@
 import ListAllRecords from "@/components/ListAllRecords";
 import { RootState } from "@/store/store";
+import { logout } from "@/store/userSlice";
+import { Button, VStack } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -7,10 +9,24 @@ import { useDispatch, useSelector } from "react-redux";
 export default function Admin() {
   const router = useRouter();
   const user = useSelector((state: RootState) => state.user);
+  const dispatch = useDispatch();
   useEffect(() => {
     if (!user.isLoggedIn || !user.username) {
       router.push("/login");
     }
   }, [user, router]);
-  return <>{user.username && <ListAllRecords />}</>;
+  return (
+    <VStack>
+      <Button
+        colorPalette={"red"}
+        onClick={() => {
+          dispatch(logout());
+          router.push("/login");
+        }}
+      >
+        Log out
+      </Button>
+      {user.username && <ListAllRecords />}
+    </VStack>
+  );
 }
